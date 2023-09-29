5c9528510b0f943eabe4208eb46a60ae0c44ff14
Alumno <Alumno@LAB1509-10>
2023-09-29 17:03:37 -0500

    Agregar archivos de proyecto.

---
+++ Lab04/Lab04/CarouselPage1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab04
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CarouselPage1 : ContentPage
+    {
+        public CarouselPage1()
+        {
+            InitializeComponent();
+        }
+    }
+}
